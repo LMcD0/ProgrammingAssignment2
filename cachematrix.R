c120b1bf991aadc3196fc169ddf1dd5ec20bfe6a
lmcd0 <landon.mcdougald@countryfinancial.com>
2018-03-06 19:07:39 -0600

    First commit test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Gets and Sets that stores a Matrix and it's inverse
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = numeric()) { 
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  
+  getinv <- function() i
+  setinv <- function(r) i <<- r
+  
+  list (set=set, get=get, getinv=getinv, setinv=setinv)
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## calculates the inverse of matrix x.  Will use a cashed solution if available 
+cacheSolve <- function(x, ...) { 
+  m <- x$getinv
+  print (m)   
+  if (!is.null(m)) {
+    message ("Using cashed data")
+    return (m)
+  }
+  else{
+    gotx <- x$get
+    ##print(gotx)
+    i <- solve(gotx)
+    x$setinv(i)
+  }
+  i
+  ## Return a matrix that is the inverse of 'x'
+  
+} 
